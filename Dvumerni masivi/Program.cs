69ce7711ebb61add2dcfd7ba71294e3d34fd70c1
pramataroff <milenpramatarov@gmail.com>
2024-04-28 23:30:59 +0300

    .

---
+++ Dvumerni masivi/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Dvumerni_masivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rows:");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Enter colona:");
+            int col = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[row, col];
+            for     (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
